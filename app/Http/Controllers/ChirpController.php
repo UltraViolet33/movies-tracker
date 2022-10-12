0021d6a333dd9c7a24970aaa7550249f3fa8ac92
Ulysse Valdenaire <ulysse.valdenairepro@gmail.com>
2022-10-12 10:35:51 +0200

    displaying chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -14,8 +14,9 @@ class ChirpController extends Controller
      */
     public function index()
     {
+        $latestChirps = Chirp::with('user')->latest()->get();
 
-        return view('chirps.index');
+        return view('chirps.index', ['chirps' => $latestChirps]);
     }
 
     /**
