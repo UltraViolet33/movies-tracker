00f99304e360105627801438fe52a98418e5fe32
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-02-21 18:28:50 +0100

    add movie model controller and migrations

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\MovieController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get("/movies", [MovieController::class, "index"])->middleware(["auth", "verified"]);
 
 
 Route::get('/dashboard', function () {
